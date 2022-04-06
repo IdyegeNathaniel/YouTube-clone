45e5ae6d423b3c3cffde119477fe7df2fea3c8dd
Idyege Nathaniel <98049103+IdyegeNathaniel@users.noreply.github.com>
2022-04-07 00:40:41 +0100

    Update Video.js

--- Video.js
+++ Video.js
@@ -10,7 +10,85 @@ function Video() {
         {/* Videos */}
         <div className="row">
             <Singlevideo
-             thumb__img="https://i.ytimg.com/vi/hdI2bqOjy3c/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA2s0gfEIumVo3Q-Duq1S17FVYQfA"
+             thumb__img="https://i.ytimg.com/vi/J6rVaFzOEP8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAcgLstj29ZgrSNzyVX0Ccwl4LFhA"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s68-c-k-c0x00ffffff-no-rj"
+             title="Guild To Becoming A Self Taught Software Developer..."
+             channel__name="Traversy Media"
+             views="21k views ."
+             timestamps=" 1 week ago"
+             videos__duration="1:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/Pv46-TvZQJk/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCwe0Io4m1NLxTvqwGaOiQIUn6Xuw"
+             profile__img="https://yt3.ggpht.com/KtRVDKFfXO96nEoglJ_epy1clktDJNKJjEPtNmvIBsNLuCi9yGMuyK3MywMiqJadqsFPxJbAhA=s68-c-k-c0x00ffffff-no-rj"
+             title="🟢 Learn  Expo React native with Facebook..."
+             channel__name=" Programmer Shahriar Sajeeb "
+             views="14k views ."
+             timestamps=" 2 days ago"
+             videos__duration="2:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/WTc2xXcJFwU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA73w5cqp985_RNGYfS5NFuwkw6uw"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLTZzZv60B1v76eOC7TsEYZD_TMH2-H5KeYxkfXGBQ=s68-c-k-c0x00ffffff-no-rj"
+             title="Tekken: Bloodline Official teaser / Netflix..."
+             channel__name="Netlfix"
+             views="21k views ."
+             timestamps=" 1 hour ago"
+             videos__duration="1:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/J6rVaFzOEP8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAcgLstj29ZgrSNzyVX0Ccwl4LFhA"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s68-c-k-c0x00ffffff-no-rj"
+             title="Guild To Becoming A Self Taught Software Developer..."
+             channel__name="Traversy Media"
+             views="21k views ."
+             timestamps=" 1 week ago"
+             videos__duration="1:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/BUCiSSyIGGU/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBYc9kQU1xI0C7JU3kOZ-Go36kApw"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s68-c-k-c0x00ffffff-no-rj"
+             title="Guild To Becoming A Self Taught Software Developer..."
+             channel__name="Traversy Media"
+             views="21k views ."
+             timestamps=" 1 week ago"
+             videos__duration="1:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/J6rVaFzOEP8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAcgLstj29ZgrSNzyVX0Ccwl4LFhA"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s68-c-k-c0x00ffffff-no-rj"
+             title="Guild To Becoming A Self Taught Software Developer..."
+             channel__name="Traversy Media"
+             views="21k views ."
+             timestamps=" 1 week ago"
+             videos__duration="1:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/7WU__JQuB8g/hqdefault.jpg?sqp=-oaymwEcCOADEI4CSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBDxb0wm6nTNysAZAKvaB6OMsIfUQ"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLQRJe3J6WEeNcz7IkMB1Ii13IFz7gORLbYYVpCGSw=s68-c-k-c0x00ffffff-no-rj"
+             title="Asake - Sungba (Remix) (Official Video) (feat. Burna..."
+             channel__name="ASAKE"
+             views="981k views ."
+             timestamps=" 3 days ago"
+             videos__duration="3:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/J6rVaFzOEP8/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLAcgLstj29ZgrSNzyVX0Ccwl4LFhA"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLSxHOOxxa9Af8Bfb2XMop3lm4tor9bViWiC-d5aaw=s68-c-k-c0x00ffffff-no-rj"
+             title="Guild To Becoming A Self Taught Software Developer..."
+             channel__name="Traversy Media"
+             views="21k views ."
+             timestamps=" 1 week ago"
+             videos__duration="1:39:32"
+            />
+            <Singlevideo
+             thumb__img="https://i.ytimg.com/vi/WTc2xXcJFwU/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLA73w5cqp985_RNGYfS5NFuwkw6uw"
+             profile__img="https://yt3.ggpht.com/ytc/AKedOLTZzZv60B1v76eOC7TsEYZD_TMH2-H5KeYxkfXGBQ=s68-c-k-c0x00ffffff-no-rj"
+             title="Tekken: Bloodline Official teaser / Netflix..."
+             channel__name="Netlfix"
+             views="21k views ."
+             timestamps=" 1 hour ago"
+             videos__duration="1:39:32"
             />
         </div>
     </div>
